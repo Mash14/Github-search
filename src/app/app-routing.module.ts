7186d82ca8edb2bf92b64e6112a97c112556154d
mash <mashalonzo741@gmail.com>
2021-08-31 14:58:41 +0300

    Adjust the navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'search', component: SearchComponent},
+  {path: 'home', component: HomeComponent}, 
   {path: 'about', component: AboutComponent},
-  {path: '**', component: NotFoundComponent },
+  
   {path: '', redirectTo:"/home",pathMatch:"full"}
 ];
 
